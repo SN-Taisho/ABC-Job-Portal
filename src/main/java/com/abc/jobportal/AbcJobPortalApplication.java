e58c4e506082d24cfe22bd6db7d0ed19438fd8d3
Anzel <112552105+SN-Taisho@users.noreply.github.com>
2023-07-27 13:10:18 +0800

    Project Creation

---
+++ src/main/java/com/abc/jobportal/AbcJobPortalApplication.java
@@ -0,0 +1,13 @@
+package com.abc.jobportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcJobPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbcJobPortalApplication.class, args);
+	}
+
+}
